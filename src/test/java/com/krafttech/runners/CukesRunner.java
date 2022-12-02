16abd9321627a3ed3b8b1b58a15e21fb32fb47d9
FTosun21 <tosunfatih9.2@gmail.com>
2022-12-02 15:06:43 +0300

    add

--- src/test/java/com/krafttech/runners/CukesRunner.java
+++ src/test/java/com/krafttech/runners/CukesRunner.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
                 features = "src/test/resources/features",
                 glue = "com/krafttech/step_definitions"
-
                 )
 
 
